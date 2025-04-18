6a0440518521caa01ea25c5b7e2f5dc26b906ce3
demo-cqsvr <66979397+demo-cqsvr@users.noreply.github.com>
2025-04-18 14:57:36 +0900

    quote

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,3 +1,14 @@
 export default function decorate(block) {
-    block.innerHTML = `<em>${block.innerHTML}</em>`;
-}
+    // decorate the quote as a blockquote element
+    const quoteDiv = block.querySelector(':scope > div > div');
+    const blockquote = document.createElement('blockquote');
+    blockquote.innerHTML = `${quoteDiv.innerHTML}`;
+    quoteDiv.parentElement.replaceWith(blockquote);
+    // decorate the author
+    const authorDiv = block.querySelector(':scope > div > div');
+    if (authorDiv) {
+      const p = document.createElement('p');
+      p.innerHTML = `<em> - ${authorDiv.innerText}</em>`;
+      authorDiv.parentElement.replaceWith(p);
+    }
+  }
